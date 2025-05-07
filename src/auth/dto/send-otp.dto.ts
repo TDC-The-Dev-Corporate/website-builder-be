acc4eb8106bb809c3f6a93fdfaef208e0627328e
Mahroosh <mahroosh16hashmi@gmail.com>
2025-05-07 11:23:25 +0500

    resend otp dto error message fix

--- src/auth/dto/send-otp.dto.ts
+++ src/auth/dto/send-otp.dto.ts
@@ -1,13 +1,12 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class SendOTPDto {
   @ApiProperty({
-    description: 'Email address of the user who forgot the password',
-    example: 'user@example.com',
+    description: "Email address of the user who forgot the password",
+    example: "user@example.com",
+    required: true,
   })
-  @IsEmail({}, { message: 'Invalid Email' })
-  @IsNotEmpty()
+  @IsEmail({}, { message: "Invalid Email" })
   email: string;
-
 }
