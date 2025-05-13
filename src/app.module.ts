cdb35870f76319e5543d70c118bea895a9f61081
Mahroosh <mahroosh16hashmi@gmail.com>
2025-05-13 17:12:26 +0500

    code clean up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,10 @@
 import { Module } from "@nestjs/common";
 import { PortfoliosModule } from "./portfolios/portfolios.module";
-import { TemplatesModule } from "./templates/templates.module";
 import { PrismaModule } from "./prisma/prisma.module";
 import { AuthModule } from "./auth/auth.module";
 import { UserModule } from "./user/user.module";
 
 @Module({
-  imports: [
-    PrismaModule,
-    PortfoliosModule,
-    TemplatesModule,
-    AuthModule,
-    UserModule,
-  ],
+  imports: [PrismaModule, PortfoliosModule, AuthModule, UserModule],
 })
 export class AppModule {}
