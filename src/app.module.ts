ae20e884c4e1937337eb5985b5def9e3fc7dde85
Mahroosh <mahroosh16hashmi@gmail.com>
2025-06-23 16:20:48 +0500

    adding health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,10 @@ import { PrismaModule } from "./prisma/prisma.module";
 import { AuthModule } from "./auth/auth.module";
 import { UserModule } from "./user/user.module";
 import { StripeModule } from "./stripe/stripe.module";
+import { AppController } from "./app.controller";
 
 @Module({
+  controllers: [AppController],
   imports: [
     PrismaModule,
     PortfoliosModule,
